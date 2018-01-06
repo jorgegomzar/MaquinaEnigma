8d6b549662e2f8427f8bdaeba85ae7b3c63ccae9
Jarvis <jorge.gomzar@gmail.com>
2018-01-06 22:48:46 +0100

    Funciones que contienen los menús y que llaman al resto de funciones

---
+++ main/menus.c
@@ -0,0 +1,58 @@
+#include "menus.h"
+#include "../auxiliar/auxiliar.h"
+
+char menuPrincipal(void)
+{
+    /**DIBUJA EL MENU PRINCIPAL Y ESPERA LA RESPUESTA DEL USUARIO*/
+    int n;
+    char c;
+	
+	
+	
+    system("title Proyecto Integrador v3.2");
+    system("color 4f");
+    system("mode con cols=50 lines=12");
+    printf(" %c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n",218,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,191);
+    printf(" %c",179);
+    setlocale(LC_CTYPE, "spanish");
+    printf("\t\t  Menú Principal\t\t");
+    setlocale(LC_CTYPE, "C");
+    printf("%c\n",179);
+    printf(" %c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n",192,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,217);
+    printf(" %c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n",218,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,191);
+    printf(" %c 1. Cifrar/descifrar un mensaje\t\t%c\n %c 2. Generar el libro de claves\t\t%c\n %c 0. Fin del Programa\t\t\t\t%c\n",179,179,179,179,179,179);
+    printf(" %c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n",192,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,217);
+    printf(" %c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n",218,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,191);
+    printf(" %c ",179);
+    setlocale(LC_CTYPE, "spanish");
+    printf("Selecciona una opción:\t\t\t");
+    setlocale(LC_CTYPE, "C");
+    printf("%c\n",179);
+    printf(" %c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n",192,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,196,217);
+    gotoxy(26,9);
+
+    do
+    {
+        c=getch();
+        n=(int)(c-48);
+    }
+    while (n>2 || n<0);
+    return c;
+}
+
+void gestionMenuPrincipal(void)
+{
+    switch(menuPrincipal())
+    {
+    case '1':
+        inicCifrado();
+        break;
+    case '2':
+        generaLibroClaves();
+        break;
+    case '0':
+        exit(0);
+        break;
+
+    }
+}
