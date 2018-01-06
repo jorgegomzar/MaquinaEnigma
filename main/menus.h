fd17bbb8342ab171c798d6204701dc133510885a
Jarvis <jorge.gomzar@gmail.com>
2018-01-06 22:49:14 +0100

    Archivo de cabecera para las funciones del archivo menus.c y mainEnigma.c

---
+++ main/menus.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <conio.h>
+#include <locale.h>
+#include <windows.h>
+char menuPrincipal(void);
+void gestionMenuPrincipal(void);
+void generaLibroClaves();
+void inicCifrado();
