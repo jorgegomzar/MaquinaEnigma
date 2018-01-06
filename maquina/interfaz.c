a6260d3e2f22e82c97b9f927c122253174f8dd0c
Jarvis <jorge.gomzar@gmail.com>
2018-01-06 22:53:24 +0100

    Funciones encargadas del aspecto visual de la máquina

---
+++ maquina/interfaz.c
@@ -0,0 +1,655 @@
+#include "interfaz.h"
+#include "../auxiliar/auxiliar.h"
+
+void dibujaInterfazM3()
+{
+    /**DIBUJA LA INTERFAZ*/
+    int i, k;
+
+
+    system("title Proyecto Integrador v3.0");
+    system("mode con cols=85 lines=37");
+    system("color 4f");
+    setlocale(LC_CTYPE,"C");
+    printf("%c",201);
+    for(i=0; i<27; i++)
+    {
+        printf("%c",205);
+    }
+
+    setlocale(LC_CTYPE,"Spanish");
+    printf(" M Á Q U I N A   E N I G M A M3");
+    setlocale(LC_CTYPE,"C");
+    for(i=0; i<25; i++)
+        printf("%c",205);
+    printf("%c",187);
+    printf("%c  U     %c%c%c     %c%c%c     %c%c%c    E           ",186,218,196,191,218,196,191,218,196,191);
+    printf("%c",218);
+    for(i=0; i<16; i++)
+    {
+        printf("%c",196);
+    }
+    printf("%c%c%c%c                   %c",191,218,196,191,186);
+    printf("%c  K     %c %c     %c %c     %c %c    T   STE-    %c",186,179,179,179,179,179,179,179);
+
+    printf("Introduce letra ");
+
+    printf("%c%c %c  [ESC %c%c fin]     %c",179,179,179,175,175,186);
+
+    printf("%c  W     %c%c%c     %c%c%c     %c%c%c    W   CKER    ",186,192,196,217,192,196,217,192,196,217);
+    printf("%c",192);
+    for(i=0; i<16; i++)
+    {
+        printf("%c",196);
+    }
+    printf("%c%c%c%c                   %c",217,192,196,217,186);
+
+    printf("%c%c%c%c%c%c",186,218,196,196,196,191);
+    for(i=0; i<3; i++)
+    {
+        printf(" %c%c%c%c%c%c%c",218,196,196,196,196,196,191);
+    }
+    printf(" %c%c%c",218,196,191);
+    printf(" %c%c%c%c%c%c%c",218,196,196,196,196,196,191);
+    for(i=0; i<42; i++)
+        printf(" ");
+    printf("%c",186);
+
+
+
+    printf("%c%c   %c",186,179,179);
+    for(i=0; i<3; i++)
+    {
+        printf(" %c     %c",179,179);
+    }
+    printf(" %c %c",179, 179);
+    printf(" %c     %c",179,179);
+    printf("  ");
+    printf("%c",218);
+    for(i=0; i<36; i++)
+        printf("%c",196);
+    printf("%c  ",191);
+    printf("%c",186);
+
+
+    printf("%c%c   %c",186,179,179);
+    for(i=0; i<3; i++)
+    {
+        printf(" %c     %c",179,179);
+    }
+    printf(" %c %c",179, 179);
+    printf(" %c     %c",179,179);
+    printf("  ");
+    printf("%c",179);
+    for(i=0; i<12; i++)
+        printf(" ");
+    printf("Texto plano ");
+    for(i=0; i<12; i++)
+        printf(" ");
+    printf("%c  ",179);
+    printf("%c",186);
+
+
+
+    printf("%c%c   %c",186,179,179);
+    for(i=0; i<3; i++)
+    {
+        printf(" %c     %c",179,179);
+    }
+    printf(" %c %c",179, 179);
+    printf(" %c     %c",179,179);
+    printf("  ");
+    printf("%c",195);
+    for(i=0; i<36; i++)
+        printf("%c",196);
+    printf("%c  ",180);
+    printf("%c",186);
+
+    for(k=0; k<10; k++)
+    {
+        printf("%c%c   %c",186,179,179);
+        for(i=0; i<3; i++)
+        {
+            printf(" %c     %c",179,179);
+        }
+        printf(" %c %c",179, 179);
+        printf(" %c     %c",179,179);
+        printf("  ");
+        printf("%c",179);
+        for(i=0; i<36; i++)
+            printf(" ");
+        printf("%c  ",179);
+        printf("%c",186);
+    }
+
+
+    printf("%c%c   %c",186,179,179);
+    for(i=0; i<3; i++)
+    {
+        printf(" %c     %c",179,179);
+    }
+    printf(" %c %c",179, 179);
+    printf(" %c     %c",179,179);
+    printf("  ");
+    printf("%c",192);
+    for(i=0; i<36; i++)
+        printf("%c",196);
+    printf("%c  ",217);
+    printf("%c",186);
+
+
+    printf("%c%c   %c",186,179,179);
+    for(i=0; i<3; i++)
+    {
+        printf(" %c     %c",179,179);
+    }
+    printf(" %c %c",179, 179);
+    printf(" %c     %c",179,179);
+    printf("  ");
+
+    for(i=0; i<40; i++)
+        printf(" ");
+
+    printf("%c",186);
+
+
+    printf("%c%c   %c",186,179,179);
+    for(i=0; i<3; i++)
+    {
+        printf(" %c     %c",179,179);
+    }
+    printf(" %c %c",179, 179);
+    printf(" %c     %c",179,179);
+    printf("  ");
+    printf("%c",218);
+    for(i=0; i<36; i++)
+        printf("%c",196);
+    printf("%c  ",191);
+    printf("%c",186);
+
+
+
+    printf("%c%c   %c",186,179,179);
+    for(i=0; i<3; i++)
+    {
+        printf(" %c     %c",179,179);
+    }
+    printf(" %c %c",179, 179);
+    printf(" %c     %c",179,179);
+    printf("  ");
+    printf("%c",179);
+    for(i=0; i<12; i++)
+        printf(" ");
+    printf("Texto cifrado");
+    for(i=0; i<11; i++)
+        printf(" ");
+    printf("%c  ",179);
+    printf("%c",186);
+
+
+    printf("%c%c   %c",186,179,179);
+    for(i=0; i<3; i++)
+    {
+        printf(" %c     %c",179,179);
+    }
+    printf(" %c %c",179, 179);
+    printf(" %c     %c",179,179);
+    printf("  ");
+    printf("%c",195);
+    for(i=0; i<36; i++)
+        printf("%c",196);
+    printf("%c  ",180);
+    printf("%c",186);
+
+    for(k=0; k<8; k++)
+    {
+        printf("%c%c   %c",186,179,179);
+        for(i=0; i<3; i++)
+        {
+            printf(" %c     %c",179,179);
+        }
+        printf(" %c %c",179, 179);
+        printf(" %c     %c",179,179);
+        printf("  ");
+        printf("%c",179);
+        for(i=0; i<36; i++)
+            printf(" ");
+        printf("%c  ",179);
+        printf("%c",186);
+    }
+
+
+
+    printf("%c%c%c%c%c%c",186,192,196,196,196,217);
+    for(i=0; i<3; i++)
+    {
+        printf(" %c%c%c%c%c%c%c",192,196,196,196,196,196,217);
+    }
+    printf(" %c%c%c",192,196,217);
+    printf(" %c%c%c%c%c%c%c",192,196,196,196,196,196,217);
+    printf("  %c",179);
+    for(i=0; i<36; i++)
+        printf(" ");
+    printf("%c  ",179);
+    printf("%c",186);
+
+
+    printf("%c%c%c%c%c%c",186,218,196,196,196,191);
+    for(i=0; i<3; i++)
+    {
+        printf(" %c%c%c%c%c%c%c",218,196,196,196,196,196,191);
+    }
+    printf("            ");
+
+    printf("  %c",179);
+    for(i=0; i<36; i++)
+        printf(" ");
+    printf("%c  ",179);
+    printf("%c",186);
+
+
+
+    printf("%c%c   %c",186,179,179);
+    for(i=0; i<3; i++)
+    {
+        printf(" %c     %c",179,179);
+    }
+    printf("    ");
+    printf("        ");
+    printf("  ");
+    printf("%c",192);
+    for(i=0; i<36; i++)
+        printf("%c",196);
+    printf("%c  ",217);
+    printf("%c",186);
+
+
+    printf("%c%c%c%c%c%c",186,192,196,196,196,217);
+    for(i=0; i<3; i++)
+    {
+        printf(" %c%c%c%c%c%c%c",192,196,196,196,196,196,217);
+    }
+    printf("    ");
+    printf("       ");
+
+
+    for(i=0; i<43; i++)
+        printf(" ");
+
+    printf("%c",186);
+
+    printf("%c",200);
+    for(i=0; i<83; i++)
+    {
+        printf("%c",205);
+    }
+    printf("%c",188);
+
+}
+
+void dibujaInterfazM4()
+{
+    /**DIBUJA LA INTERFAZ PARA 4 ROTORES*/
+    int i, k;
+
+
+    system("title Proyecto Integrador v3.0");
+    system("mode con cols=93 lines=37");
+    system("color 4f");
+    setlocale(LC_CTYPE,"C");
+    printf("%c",201);
+    for(i=0; i<31; i++)
+    {
+        printf("%c",205);
+    }
+
+    setlocale(LC_CTYPE,"Spanish");
+    printf(" M Á Q U I N A   E N I G M A M4");
+    setlocale(LC_CTYPE,"C");
+    for(i=0; i<29; i++)
+        printf("%c",205);
+    printf("%c",187);
+    printf("%c  U     %c%c%c     %c%c%c     %c%c%c     %c%c%c    E           ",186,218,196,191,218,196,191,218,196,191,218,196,191);
+    printf("%c",218);
+    for(i=0; i<16; i++)
+    {
+        printf("%c",196);
+    }
+    printf("%c%c%c%c                   %c",191,218,196,191,186);
+    printf("%c  K     %c %c     %c %c     %c %c     %c %c    T   STE-    %c",186,179,179,179,179,179,179,179,179,179);
+
+    printf("Introduce letra ");
+
+    printf("%c%c %c  [ESC %c%c fin]     %c",179,179,179,175,175,186);
+
+    printf("%c  W     %c%c%c     %c%c%c     %c%c%c     %c%c%c    W   CKER    ",186,192,196,217,192,196,217,192,196,217,192,196,217);
+    printf("%c",192);
+    for(i=0; i<16; i++)
+    {
+        printf("%c",196);
+    }
+    printf("%c%c%c%c                   %c",217,192,196,217,186);
+
+    printf("%c%c%c%c%c%c",186,218,196,196,196,191);
+    for(i=0; i<4; i++)
+    {
+        printf(" %c%c%c%c%c%c%c",218,196,196,196,196,196,191);
+    }
+    printf(" %c%c%c",218,196,191);
+    printf(" %c%c%c%c%c%c%c",218,196,196,196,196,196,191);
+    for(i=0; i<42; i++)
+        printf(" ");
+    printf("%c",186);
+
+
+
+    printf("%c%c   %c",186,179,179);
+    for(i=0; i<4; i++)
+    {
+        printf(" %c     %c",179,179);
+    }
+    printf(" %c %c",179, 179);
+    printf(" %c     %c",179,179);
+    printf("  ");
+    printf("%c",218);
+    for(i=0; i<36; i++)
+        printf("%c",196);
+    printf("%c  ",191);
+    printf("%c",186);
+
+
+    printf("%c%c   %c",186,179,179);
+    for(i=0; i<4; i++)
+    {
+        printf(" %c     %c",179,179);
+    }
+    printf(" %c %c",179, 179);
+    printf(" %c     %c",179,179);
+    printf("  ");
+    printf("%c",179);
+    for(i=0; i<12; i++)
+        printf(" ");
+    printf("Texto plano ");
+    for(i=0; i<12; i++)
+        printf(" ");
+    printf("%c  ",179);
+    printf("%c",186);
+
+
+
+    printf("%c%c   %c",186,179,179);
+    for(i=0; i<4; i++)
+    {
+        printf(" %c     %c",179,179);
+    }
+    printf(" %c %c",179, 179);
+    printf(" %c     %c",179,179);
+    printf("  ");
+    printf("%c",195);
+    for(i=0; i<36; i++)
+        printf("%c",196);
+    printf("%c  ",180);
+    printf("%c",186);
+
+    for(k=0; k<10; k++)
+    {
+        printf("%c%c   %c",186,179,179);
+        for(i=0; i<4; i++)
+        {
+            printf(" %c     %c",179,179);
+        }
+        printf(" %c %c",179, 179);
+        printf(" %c     %c",179,179);
+        printf("  ");
+        printf("%c",179);
+        for(i=0; i<36; i++)
+            printf(" ");
+        printf("%c  ",179);
+        printf("%c",186);
+    }
+
+
+    printf("%c%c   %c",186,179,179);
+    for(i=0; i<4; i++)
+    {
+        printf(" %c     %c",179,179);
+    }
+    printf(" %c %c",179, 179);
+    printf(" %c     %c",179,179);
+    printf("  ");
+    printf("%c",192);
+    for(i=0; i<36; i++)
+        printf("%c",196);
+    printf("%c  ",217);
+    printf("%c",186);
+
+
+    printf("%c%c   %c",186,179,179);
+    for(i=0; i<4; i++)
+    {
+        printf(" %c     %c",179,179);
+    }
+    printf(" %c %c",179, 179);
+    printf(" %c     %c",179,179);
+    printf("  ");
+
+    for(i=0; i<40; i++)
+        printf(" ");
+
+    printf("%c",186);
+
+
+    printf("%c%c   %c",186,179,179);
+    for(i=0; i<4; i++)
+    {
+        printf(" %c     %c",179,179);
+    }
+    printf(" %c %c",179, 179);
+    printf(" %c     %c",179,179);
+    printf("  ");
+    printf("%c",218);
+    for(i=0; i<36; i++)
+        printf("%c",196);
+    printf("%c  ",191);
+    printf("%c",186);
+
+
+
+    printf("%c%c   %c",186,179,179);
+    for(i=0; i<4; i++)
+    {
+        printf(" %c     %c",179,179);
+    }
+    printf(" %c %c",179, 179);
+    printf(" %c     %c",179,179);
+    printf("  ");
+    printf("%c",179);
+    for(i=0; i<12; i++)
+        printf(" ");
+    printf("Texto cifrado");
+    for(i=0; i<11; i++)
+        printf(" ");
+    printf("%c  ",179);
+    printf("%c",186);
+
+
+    printf("%c%c   %c",186,179,179);
+    for(i=0; i<4; i++)
+    {
+        printf(" %c     %c",179,179);
+    }
+    printf(" %c %c",179, 179);
+    printf(" %c     %c",179,179);
+    printf("  ");
+    printf("%c",195);
+    for(i=0; i<36; i++)
+        printf("%c",196);
+    printf("%c  ",180);
+    printf("%c",186);
+
+    for(k=0; k<8; k++)
+    {
+        printf("%c%c   %c",186,179,179);
+        for(i=0; i<4; i++)
+        {
+            printf(" %c     %c",179,179);
+        }
+        printf(" %c %c",179, 179);
+        printf(" %c     %c",179,179);
+        printf("  ");
+        printf("%c",179);
+        for(i=0; i<36; i++)
+            printf(" ");
+        printf("%c  ",179);
+        printf("%c",186);
+    }
+
+
+
+    printf("%c%c%c%c%c%c",186,192,196,196,196,217);
+    for(i=0; i<4; i++)
+    {
+        printf(" %c%c%c%c%c%c%c",192,196,196,196,196,196,217);
+    }
+    printf(" %c%c%c",192,196,217);
+    printf(" %c%c%c%c%c%c%c",192,196,196,196,196,196,217);
+    printf("  %c",179);
+    for(i=0; i<36; i++)
+        printf(" ");
+    printf("%c  ",179);
+    printf("%c",186);
+
+
+    printf("%c%c%c%c%c%c",186,218,196,196,196,191);
+    for(i=0; i<4; i++)
+    {
+        printf(" %c%c%c%c%c%c%c",218,196,196,196,196,196,191);
+    }
+    printf("            ");
+
+    printf("  %c",179);
+    for(i=0; i<36; i++)
+        printf(" ");
+    printf("%c  ",179);
+    printf("%c",186);
+
+
+
+    printf("%c%c   %c",186,179,179);
+    for(i=0; i<4; i++)
+    {
+        printf(" %c     %c",179,179);
+    }
+    printf("    ");
+    printf("        ");
+    printf("  ");
+    printf("%c",192);
+    for(i=0; i<36; i++)
+        printf("%c",196);
+    printf("%c  ",217);
+    printf("%c",186);
+
+
+    printf("%c%c%c%c%c%c",186,192,196,196,196,217);
+    for(i=0; i<4; i++)
+    {
+        printf(" %c%c%c%c%c%c%c",192,196,196,196,196,196,217);
+    }
+    printf("    ");
+    printf("       ");
+
+
+    for(i=0; i<43; i++)
+        printf(" ");
+
+    printf("%c",186);
+
+    printf("%c",200);
+    for(i=0; i<91; i++)
+    {
+        printf("%c",205);
+    }
+    printf("%c",188);
+
+}
+
+void 	muestraEsquema (struct sEnigma enigma, struct sPunto coordRotor[])
+{
+    /**ACTUALIZA LA INTERFAZ CON TODAS LAS CADENAS DE ROTORES Y ANILLOS*/
+    int i;
+
+    if(enigma.config.numRotores==3)
+        dibujaEstator(0);
+    else    dibujaEstator(8);
+    if(enigma.config.numRotores==3)
+        dibujaStecker(enigma.config.stecker, 0);
+    else    dibujaStecker(enigma.config.stecker, 8);
+
+    if(enigma.config.numRotores==4) /**SI HAY 4 ROTORES LO DIBUJA DE UNA FORMA(COLOCANDO EL 4 ROTOR EN LA PRIMERA POSICION)*/
+    {
+        for(i=0; i<enigma.config.numRotores; i++)
+        {
+            if(i!=3)
+                dibujaRotor(enigma.listaRotores.rotor[i], coordRotor[i+1]);
+            else    dibujaRotor(enigma.listaRotores.rotor[i], coordRotor[3-i]);
+        }
+
+    }
+    else
+    {
+        for(i=0; i<enigma.config.numRotores; i++)/**SI HAY 3 ROTORES LO DIBUJA DE OTRA FORMA*/
+        {
+            dibujaRotor(enigma.listaRotores.rotor[i], coordRotor[i]);
+        }
+    }
+
+    dibujaReflector(enigma.reflector);
+    gotoxy(3,33);
+    printf("%c", enigma.config.modeloETW);
+}
+
+void dibujaEstator(unsigned short dx)
+{
+    /**MUESTRA EL ESTATOR EN LA INTERFAZ*/
+    int i, y=5;
+    for(i=0; i<26; i++)/*DIBUJO EL ESTATOR*/
+    {
+        gotoxy(32+dx,y);
+        printf("%c",i+65);
+        y++;
+    }
+
+}
+
+void dibujaStecker(char stecker[], unsigned short dx)
+{
+    /**MUESTRA STECKER POR PANTALLA*/
+    int i, y=5;
+    for(i=0; i<26; i++) /*DIBUJO EL RECUADRO STECKER*/
+    {
+        gotoxy(36+dx,y);
+        printf("%c",i+65);
+        gotoxy(39+dx,y);
+        printf("%c", stecker[i]);
+        gotoxy(40+dx,y);
+        printf("%c",i+65);
+        y++;
+    }
+}
+unsigned short printLetraPlana(char letra, FILE *ptPlano)
+{
+    /**SE ESCRIBEN LAS TECLAS INTRODUCIDAS POR EL USUARIO EN PANTALLA Y EN UN ARCHIVO*/
+    int valor=0;
+    printf("%c", letra);
+    fprintf(ptPlano, "%c", letra);
+    valor++;
+
+    return valor;
+}
+
+void printLetraCifrada(char letra, FILE* ptCifrado)
+{
+    /**SE ESCRIBEN LAS TECLAS YA CIFRADAS EN PANTALLA Y EN UN ARCHIVO*/
+    printf("%c",letra);
+    fprintf(ptCifrado, "%c", letra);
+}
