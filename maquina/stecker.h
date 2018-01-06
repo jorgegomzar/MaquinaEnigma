a3cbcec7a51912f168612bfaca4b4ac9154a59b9
Jarvis <jorge.gomzar@gmail.com>
2018-01-06 22:57:11 +0100

    Archivo de cabecera para las funciones del fichero stecker.c

---
+++ maquina/stecker.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+void 	dibujaStecker (char stecker[]);
+unsigned short 	opStecker (unsigned short posIn, char stecker[]);
+void 	fprintStecker (char conex[], FILE *ptr, FILE *pCfg);
+void 	genStecker (char conexiones[]);
+void    gotoxy(int x, int y);
